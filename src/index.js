8d148712c9d1b2bac563083ffd88bd6834b5c2ec
TP1997 <thanhphamlike@gmail.com>
2021-07-23 23:01:17 -0400

    common src files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import header from "./_header";
-
+import navbar from "./_navbar";
+import '../dist/css/main.css'
 function main(){
   const CONTENT = document.querySelector('.content');
   const myHeader = header();
-
+  const myNavBar = navbar();
   CONTENT.appendChild(myHeader);
+  CONTENT.appendChild(myNavBar);
   
 }
 
