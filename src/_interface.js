2e28367c2ac44dc95e52c0934b7f109679b190f4
TP1997 <thanhphamlike@gmail.com>
2021-08-12 21:22:05 -0400

    Added proper evenListener for four status-btns

--- src/_interface.js
+++ src/_interface.js
@@ -11,13 +11,13 @@ function getData() {
   // get data (create default data here to demo)
   let todoList1 = new todoList('My First List');
   let todoItem1 = new todoItem('Item 1', 'Description 1', new Date(2022, 1, 1));
-  let todoItem2 = new todoItem('Item 2', 'Description 2', new Date(2022, 1, 1));
+  let todoItem2 = new todoItem('Item 2', 'Description 2', new Date());
   todoItem1.setIsDone(true);
   todoList1.add(todoItem1);
   todoList1.add(todoItem2);
 
   let todoList2 = new todoList('My Second List');
-  let todoItem3 = new todoItem('Item 3', 'Description 3', new Date(2022, 1, 1));
+  let todoItem3 = new todoItem('Item 3', 'Description 3', new Date());
   let todoItem4 = new todoItem('Item 4', 'Description 4', new Date(2022, 1, 1));
   todoItem4.setIsDone(true);
   todoList2.add(todoItem3);
@@ -32,16 +32,41 @@ function getData() {
 function allProjectsBtn() {
   const ALL_BTN = document.querySelector('.status__all');
   const TODO_CONTAINER = document.querySelector('.todoContainer');
+  const NEWTODO_BTN = document.querySelector('.todoContainer__add');
   ALL_BTN.addEventListener('click', (e) => {
     clearTodoContainer();
-    TODO_CONTAINER.removeChild(TODO_CONTAINER.firstChild);
+    NEWTODO_BTN.classList.remove('show');
+    // if (TODO_CONTAINER.hasChildNodes()) TODO_CONTAINER.removeChild(TODO_CONTAINER.firstChild);
     let lists = projects.getProjects();
     lists.forEach((list) => {
       let items = list.getItems();
       items.forEach((item) => {
         let div = simpleHtmlTag.makeDiv('todoItem');
-        div.innerHTML = item.getName() + '----' + item.getDescription();
+        div.innerHTML = item.getName() + '----' + item.getDescription() + '----' + item.getDueDate();
         TODO_CONTAINER.appendChild(div);
+      })
+    })
+  })
+}
+
+function todayProjectsBtn() {
+  const TODAY_BTN = document.querySelector('.status__today');
+  const TODO_CONTAINER = document.querySelector('.todoContainer');
+  const NEWTODO_BTN = document.querySelector('.todoContainer__add');
+  TODAY_BTN.addEventListener('click', (e) => {
+    clearTodoContainer();
+    NEWTODO_BTN.classList.remove('show');
+    let lists = projects.getProjects();
+    lists.forEach((list) => {
+      let items = list.getItems();
+      items.forEach((item) => {
+        let dueDate = item.getDueDate();
+        let today = new Date();
+        if (dueDate.getDate() === today.getDate() && dueDate.getMonth() === today.getMonth() && dueDate.getYear() === today.getYear()) {
+          let div = simpleHtmlTag.makeDiv('todoItem');
+          div.innerHTML = item.getName() + '----' + item.getDescription() + '----' + item.getDueDate();
+          TODO_CONTAINER.appendChild(div);
+        }
       })
     })
   })
@@ -50,16 +75,17 @@ function allProjectsBtn() {
 function finishedProjectsBtn() {
   const FINISHED_BTN = document.querySelector('.status__finished');
   const TODO_CONTAINER = document.querySelector('.todoContainer');
+  const NEWTODO_BTN = document.querySelector('.todoContainer__add');
   FINISHED_BTN.addEventListener('click', (e) => {
     clearTodoContainer();
-    TODO_CONTAINER.removeChild(TODO_CONTAINER.firstChild);
+    NEWTODO_BTN.classList.remove('show');
     let lists = projects.getProjects();
     lists.forEach((list) => {
       let items = list.getItems();
       items.forEach((item) => {
         if (item.getIsDone()) {
           let div = simpleHtmlTag.makeDiv('todoItem');
-          div.innerHTML = item.getName() + '----' + item.getDescription();
+          div.innerHTML = item.getName() + '----' + item.getDescription() + '----' + item.getDueDate();
           TODO_CONTAINER.appendChild(div);
         }
       })
@@ -70,16 +96,17 @@ function finishedProjectsBtn() {
 function unfinishedProjectsBtn() {
   const UNFINISHED_BTN = document.querySelector('.status__notFinised');
   const TODO_CONTAINER = document.querySelector('.todoContainer');
+  const NEWTODO_BTN = document.querySelector('.todoContainer__add');
   UNFINISHED_BTN.addEventListener('click', (e) => {
     clearTodoContainer();
-    TODO_CONTAINER.removeChild(TODO_CONTAINER.firstChild);
+    NEWTODO_BTN.classList.remove('show');
     let lists = projects.getProjects();
     lists.forEach((list) => {
       let items = list.getItems();
       items.forEach((item) => {
         if (!item.getIsDone()) {
           let div = simpleHtmlTag.makeDiv('todoItem');
-          div.innerHTML = item.getName() + '----' + item.getDescription();
+          div.innerHTML = item.getName() + '----' + item.getDescription() + '----' + item.getDueDate();
           TODO_CONTAINER.appendChild(div);
         }
       })
@@ -94,13 +121,12 @@ function addProjectBtn() {
   ADD_PROJECT_BUTTON.addEventListener('click', (e) => {
     ADD_PROJECT_FORM.classList.toggle('show');
     ADD_PROJECT_FORM.firstChild.value = '';
-    ADD_PROJECT_FORM.firstChild.autofocus = true;
   })
 }
 
 function confirmBtnForm() {
-  const confirmBtn = document.querySelector('.project__confirm');
-  confirmBtn.addEventListener('click', (e) => {
+  const CONFIRM_BTN = document.querySelector('.project__confirm');
+  CONFIRM_BTN.addEventListener('click', (e) => {
     e.preventDefault();
     let name = e.target.parentNode.firstChild.value || 'default name';
     let list = new todoList(name);
@@ -117,10 +143,57 @@ function confirmBtnForm() {
     e.target.parentNode.classList.toggle('show');
   })
 }
+
+function showTodoLists() {
+  const PROJECT = document.querySelector('.project');
+  const TODO_CONTAINER = document.querySelector('.todoContainer');
+  let lists = projects.getProjects();
+  lists.forEach((list, index) => {
+    let newList = simpleHtmlTag.makeTag('button', 'project__list');
+    newList.setAttribute('data-projectName', list.getName());
+    newList.setAttribute('data-index', index);
+    newList.innerHTML = list.getName();
+    PROJECT.appendChild(newList)
+    newList.addEventListener('click', (e)=>{
+      const NEWTODO_BTN = document.querySelector('.todoContainer__add');
+      clearTodoContainer();
+      NEWTODO_BTN.classList.add('show');
+      let listIndex = +(e.target.getAttribute('data-index'));
+      let list = projects.getProject(listIndex);
+      let items = list.getItems();
+      items.forEach((item) =>{
+        let div = simpleHtmlTag.makeDiv('todoItem');
+          div.innerHTML = item.getName() + '----' + item.getDescription() + '----' + item.getDueDate();
+          TODO_CONTAINER.appendChild(div);
+      })
+    })
+  })
+}
+
+// function confirmBtnForm() {
+//   const confirmBtn = document.querySelector('.project__confirm');
+//   confirmBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     let name = e.target.parentNode.firstChild.value || 'default name';
+//     let list = new todoList(name);
+//     let index = projects.add(list);
+//     console.log(list);
+//     let newProject = simpleHtmlTag.makeTag('button', 'project__list');
+//     newProject.setAttribute('data-projectName', name);
+//     newProject.setAttribute('data-index', index);
+//     newProject.innerHTML = name;
+//     e.target.parentNode.parentNode.appendChild(newProject);
+//     newProject.addEventListener('click', (e) => {
+//       projectBtnAddEvent(e.target);
+//     })
+//     e.target.parentNode.classList.toggle('show');
+//   })
+// }
+
 function clearTodoContainer() {
-  let todoContainer = document.querySelector('.todoContainer');
-  while (todoContainer.childElementCount > 1) {
-    todoContainer.removeChild(todoContainer.lastChild);
+  const TODO_CONTAINER = document.querySelector('.todoContainer');
+  while (TODO_CONTAINER.childElementCount > 1) {
+    TODO_CONTAINER.removeChild(TODO_CONTAINER.lastChild);
   };
   console.log('1');
 }
@@ -128,7 +201,6 @@ function clearTodoContainer() {
 function projectBtnAddEvent(node) {
   clearTodoContainer();
   console.log('2');
-  let todoContainer = document.querySelector('.todoContainer');
   let index = node.getAttribute('data-index');
   let project = projects.getProject(index);
   project.add(new todoItem('default todo', 'This is a default', new Date(2022, 1, 1)));
@@ -151,8 +223,10 @@ function cancelBtnForm() {
 function generalInterface() {
   getData();
   allProjectsBtn();
+  todayProjectsBtn();
   finishedProjectsBtn();
   unfinishedProjectsBtn();
+  showTodoLists();
   addProjectBtn();
   confirmBtnForm(projects);
   cancelBtnForm();
