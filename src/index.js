4e7c271e0e96ca0f63aeacf711ab0d6269a7890e
TP1997 <thanhphamlike@gmail.com>
2021-08-31 21:14:34 -0400

    The main files and index files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import header from "./_header";
-import navbar from "./_navbar";
+import { navbar} from "./_navbar";
+import { todoItem, todoContainer } from "./_todoContainer";
+import footer from "./_footer";
+import generalInterface from "./_interface";
 import '../dist/css/main.css'
-function main(){
+function main() {
   const CONTENT = document.querySelector('.content');
   const myHeader = header();
   const myNavBar = navbar();
+  const myTodoContainer = todoContainer();
+  const myFooter = footer();
+
   CONTENT.appendChild(myHeader);
   CONTENT.appendChild(myNavBar);
-  
+  CONTENT.appendChild(myTodoContainer);
+  CONTENT.appendChild(myFooter);
+
+  const myInterface = generalInterface();
+  // testing area
+  // let todo1 = new todoItem('test1', 'test1Des', new Date(2022, 1, 1));
+  // let todo2 = new todoItem('test1', 'test1Des', new Date(2022, 2, 1));
+  // let list1 = new todoList('Project 1 test');
+  // list1.add(todo1);
+  // list1.add(todo2);
+  // controlers
+  // let myProjectController = new projectController();
+  // console.log(myProjectController.getProjects());
+  // end of testing area
 }
 
 main();
